410ae43d17d08e50553dd13f2d1b1c41bad3aa99
noir <anoirsmail@gmail.com>
2024-10-22 00:16:46 +0500

    hello world config done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-import Koa from "koa";
-import { router } from "./router/routes";
+import Koa from 'koa';
+import { PrismaClient } from '@prisma/client';
+import Redis from 'ioredis';
+
+const prisma = new PrismaClient();
+const redis = new Redis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: parseInt(process.env.REDIS_PORT || '6379', 10),
+  password: process.env.REDIS_PASSWORD || 'DORIME', // Include Redis password
+});
 
 const app = new Koa();
-
 const port = 3000;
 
-app.use(router.routes());
+
+redis.set('testKey', 'Hello Redis with Auth!');
+
+app.use(async (ctx) => {
+  const redisValue = await redis.get('testKey');
+  ctx.body = `Hello World and ${redisValue}`;
+});
 
 app.listen(port, () => {
-  console.log(`🚀 Server is running on port http://localhost:${port}/`);
+  console.log(`🚀 Server is running on http://localhost:${port}`);
 });
